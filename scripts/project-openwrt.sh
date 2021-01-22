83adb2ae0975cfee76c9497cc16f57ad5e57b052
xms363 <72204373+xms363@users.noreply.github.com>
2021-01-22 17:31:12 +0800

    Update project-openwrt.sh

--- scripts/project-openwrt.sh
+++ scripts/project-openwrt.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.5/g' package/base-files/files/bin/config_generate
 
 mkdir package/community
 pushd package/community
